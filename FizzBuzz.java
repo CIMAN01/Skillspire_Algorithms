6ad3591b2ab9fef6aa423d034ca16243df8ea06d
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2020-04-10 12:21:16 -0700

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,58 +1,55 @@
-/**
- Common interview question:
- Print number 1 to 100
- if the number is divisible by 3, print Fizz
- if the number is divisible by 5, print Buzz
- if it's divisible by both, print FizzBuzz
- **/
-
 /*
-Create a fizzBuzz method that takes an integer.
-Print numbers 0 up to and including the number passed in the argument.
-BUT for each number that is divisible by 3, print "FIZZ" instead of that number.
-For each number that is divisible by 5, print "BUZZ" instead of that number.
-And for each number divisible by both 3 and 5 print "FIZZBUZZ" instead of that number.
- */
+    Create a fizzBuzz method that takes an integer. Print numbers 0 up to
+    and including the number passed in the argument. BUT for each number
+    that is divisible by 3, print "FIZZ" instead of that number. For each
+    number that is divisible by 5, print "BUZZ" instead of that number.
+    And for each number divisible by both 3 and 5 print "FIZZBUZZ" instead
+    of that number.
+
+*/
 
 public class FizzBuzz {
 
-    public static void FizzBuzz(int num){
-        // loop through num
+    /*
+        We have a for loop that iterates from 0 from up to the num
+        passed in the argument.
+    */
+    public static void fizzBuzz(int num) {
+        // "i" represents the current number between 0 and num.
         for (int i = 0; i <= num; i++) {
-            // must start with && logic to get proper output
-            if ((num % 3 == 0) && (num % 5 == 0))
-                System.out.println("FizzBuzz");
-            else if (num % 3 == 0)
-                System.out.println("Fizz");
-            else if (num % 5 == 0)
-                System.out.println("Buzz");
-            else
-                System.out.println(num);
+            // If i is divisible by 3 AND 5, print FIZZBUZZ
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FIZZBUZZ");
+            }
+            // If i is divisible by 3, print FIZZ
+            else if (i % 3 == 0) {
+                System.out.println("FIZZ");
+            }
+            // If i is divisible by 5, print BUZZ
+            else if (i % 5 == 0) {
+                System.out.println("BUZZ");
+            }
+            // Otherwise print the number that i represents
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    // LJ had a great response! He's keeping his code minimal
+    // by using ternary operators.
+    public static void fizzBuzzFromLJ(int highestNumber) {
+        for (int i = 1; i <= highestNumber; i++) {
+            String fizzCheck = "";
+            fizzCheck += (i % 3) == 0 ? "fizz" : "";
+            fizzCheck += (i % 5) == 0 ? "buzz" : "";
+            System.out.println(!fizzCheck.isEmpty() ? fizzCheck : i);
         }
     }
 
     public static void main(String[] args) {
-        FizzBuzz(15);
+        fizzBuzz(15);
+        fizzBuzzFromLJ(15);
     }
-
-/*
-Output:
-
-1
-2
-FIZZ
-4
-BUZZ
-FIZZ
-7
-8
-FIZZ
-BUZZ
-11
-FIZZ
-13
-14
-FIZZBUZZ
-         */
 
 }
