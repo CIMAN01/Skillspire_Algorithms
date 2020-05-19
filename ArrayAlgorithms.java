0825b34161098fd1bf2e84155b94197c804b32a7
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2020-05-19 14:37:57 -0700

    Update ArrayAlgorithms.java

--- ArrayAlgorithms.java
+++ ArrayAlgorithms.java
@@ -26,20 +26,35 @@ Create a method that reads the array
 
 import java.util.Arrays;
 
+// main class
 public class ArraysAlgorithm {
 
-    public static void arrayPrintEachValue(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+    // a method that takes in an array and prints each value in the array
+    public static void printEachValueInArray(int[] array) {
+        // store last index value in a variable
+        int lastIndex = array.length-1;
+        // print opening bracket
+        System.out.print("[");
+        // loop through the entire array minus the last array index
+        for (int i = 0; i < array.length-1; i++) { // stop one index short
+            // print each value and add a space/comma between them
+            System.out.print(array[i] + ", ");
         }
+        // print last element in array
+        System.out.print(array[lastIndex]);
+        // print closing bracket
+        System.out.print("]");
     }
 
-    public static void arrayPrint(int[] array) {
+    // a method that takes in an array and prints the array using Arrays.toString()
+    public static void printArray(int[] array) {
         System.out.println(Arrays.toString(array));
     }
 
     // a method that returns the highest and lowest number in an array
-    public static String arrayReturnHighLow(int[] array) {
+    public static String returnHighestLowestValueInArray(int[] array) {
+        // create a string variable and store the highest and lowest number
+        String output = "";
         // create variables to store first indexes
         int lowest = array[0];
         int highest = array[0];
@@ -52,21 +67,21 @@ public class ArraysAlgorithm {
                 highest = array[i];
             }
         }
-        // create a string variable and store the highest and lowest number
-        String output = "Highest: " + highest + "\nLowest: " + lowest;
-        // return a string
+        // use string concatenation
+        output = "Highest value: " + highest + "\nLowest value: " + lowest;
+        // return the resulting string
         return output;
     }
 
-    // a method the creates an array of size int with random values
+    // a method the creates an array of size int and fills it with random values
     public static int[] createRandomArray(int size) {
-        int[] randArray = new int[size];
+        int[] randomArray = new int[size];
         // fill array with random numbers
-        for (int i = 0; i < randArray.length; i++) {
+        for (int i = 0; i < randomArray.length; i++) {
             // use Math.random to get a random number for each index
-            randArray[i] = (int) (Math.random() * 100);
+            randomArray[i] = (int) (Math.random() * 100);
         }
-        return randArray;
+        return randomArray;
     }
 
     // a method that sorts the array from lowest to highest
@@ -88,39 +103,47 @@ public class ArraysAlgorithm {
         return array;
     }
 
-    // another way to sort an array by using built-in method
+    // another way to sort an array by using a built-in method
     public static int[] sortArray2(int[] array) {
         // use Arrays.sort() instead
         Arrays.sort(array);
         return array;
     }
 
-
     // main method
     public static void main(String[] args) {
         // create a new array with some values
         int[] someArray = {104, 32, 5, 8, 7};
 
-        // print each value in that array
-        arrayPrintEachValue(someArray);
+        System.out.println("\nPrinting each value in array: ");
+        // print each value in the array
+        printEachValueInArray(someArray);
 
         System.out.println();
+        System.out.println("\nanother way of printing the array: ");
         // another way to print the previous array
-        arrayPrint(someArray);
+        printArray(someArray);
 
         System.out.println();
         // return highest and lowest numbers in the array
-        System.out.println(arrayReturnHighLow(someArray));
+        System.out.println(returnHighestLowestValueInArray(someArray));
 
         System.out.println();
         // create a random array with 5 elements
         int[] anotherArray = createRandomArray(5);
+        System.out.println("A randomly created array: ");
         // print the newly created random array
         System.out.println(Arrays.toString(anotherArray));
 
         System.out.println();
+        System.out.println("Sorting the array from lowest to highest: ");
         // sort the array from lowest to highest number
         System.out.println(Arrays.toString(sortArray(anotherArray)));
 
+        System.out.println();
+        System.out.println("Another way of sorting the array using built-in method: ");
+        // another way to sort the previous array
+        System.out.println(Arrays.toString(sortArray2(anotherArray)));
     }
+
 }
